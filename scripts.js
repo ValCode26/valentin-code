d8cb05d72550164406e50c866f54ac388d79a5e8
Claudiu Valentin Borsan <valentinborsan@libero.it>
2025-02-27 16:00:35 +0100

    first commit

--- scripts.js
+++ scripts.js
@@ -1,10 +1,21 @@
-let menubar = document.getElementById("menu");
-menubar.style.maxHeight='0px';
-function toggle (){
-    if (menubar.style.maxHeight=='0px'){
-        menubar.style.maxHeight='200px'
-    }
-    else {
-        menubar.style.maxHeight='0px'
-    }
-}
+const toggle = document.querySelector('#toggle');
+const menu = document.querySelector('.topnav');
+const closemenu = document.querySelector('.close-menu');
+const nav = document.querySelector('nav');
+const body = document.querySelector('body');
+const hero = document.querySelector('.hero');
+
+toggle.addEventListener("click", function(){
+    menu.style.display = "flex";
+nav.style.padding = "0px";
+body.style.overflow = "hidden";
+hero.style.marginTop = "440px";
+
+});
+
+closemenu.addEventListener("click", function(){
+    menu.style.display = "none";
+    nav.style.padding = "20px 50px";
+    body.style.overflow = "auto";
+    hero.style.marginTop = "0";
+});
