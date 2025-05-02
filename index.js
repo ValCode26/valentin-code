c45efa6facb0df6cda52eabc28ece64a8a0bf4f3
Claudiu Valentin Borsan <valentinborsan@libero.it>
2025-05-02 18:50:53 +0200

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnlogin-popup');
-const iconClose = document.querySelector('.icon-close');
+const openBtn = document.getElementById("openMenu");
+const closeBtn = document.getElementById("closeMenu");
+const overlay = document.getElementById("menuOverlay");
 
-registerLink.addEventListener('click', ()=>{
-wrapper.classList.add('active');
+openBtn.addEventListener("click", () => {
+  overlay.classList.add("active");
 });
-loginLink.addEventListener('click', ()=>{
-    wrapper.classList.remove('active');
-    });
-    btnPopup.addEventListener('click', ()=>{
-        wrapper.classList.add('active-popup');
-        });
-        iconClose.addEventListener('click', ()=>{
-            wrapper.classList.remove('active-popup');
-            });
+
+closeBtn.addEventListener("click", () => {
+  overlay.classList.remove("active");
+});
