e418425b4ea15bfcdb1b85dbc88f62e02f85d3d8
Claudiu Valentin Borsan <valentinborsan@libero.it>
2025-05-03 12:33:23 +0200

    modified index.html modified index.css modified index.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const openBtn = document.getElementById("openMenu");
-const closeBtn = document.getElementById("closeMenu");
-const overlay = document.getElementById("menuOverlay");
 
-openBtn.addEventListener("click", () => {
-  overlay.classList.add("active");
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+const dropdowns = document.querySelectorAll('.dropdown');
+const overlay = document.getElementById('menuOverlay');
+
+hamburger.addEventListener('click', () => {
+  const isActive = hamburger.classList.toggle('active');
+  navLinks.classList.toggle('show');
+  overlay.classList.toggle('show');
+
+  document.body.classList.toggle('no-scroll', isActive);
 });
 
-closeBtn.addEventListener("click", () => {
-  overlay.classList.remove("active");
+overlay.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navLinks.classList.remove('show');
+  overlay.classList.remove('show');
+
+  document.body.classList.remove('no-scroll');
 });
